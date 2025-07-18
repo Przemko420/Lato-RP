320aaa357a79ad84d06b7c387a56584119b332e6
Przemko420 <oldspicecapitan420@hotmail.com>
2025-07-18 21:40:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// script.js
+
 function joinDiscord() {
   window.open("https://discord.gg/latorp", "_blank");
 }
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert("Dziękujemy za kontakt! Odezwiemy się wkrótce.");
-  this.reset();
-});
